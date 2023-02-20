291877d51ded51e80bcd71a7b50e44da715d6c22
Solomon Lambert <sol.lambert@gmail.com>
2023-02-20 14:14:40 -0600

    initial commit

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import Stats from '../Stats/Stats'
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <Stats />
+      </div>
+    )
+}
+
+export default App;
---
+++ src/Components/Stats/Stats.jsx
@@ -0,0 +1,16 @@
+import './Stats.css'
+
+function Stats() {
+    return (
+        <ul>
+            <li>Str: 1</li>
+            <li>Str: 1</li>
+            <li>Str: 1</li>
+            <li>Str: 1</li>
+            <li>Str: 1</li>
+            <li>Str: 1</li>
+        </ul>
+    )
+}
+
+export default Stats;
